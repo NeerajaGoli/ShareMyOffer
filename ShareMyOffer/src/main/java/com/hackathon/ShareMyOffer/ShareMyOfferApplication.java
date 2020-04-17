1ca1fc7e62daeef3a5a3770b36efc80d443d48ae
Neeraja <neeraja.goli@zemosolabs.com>
2020-04-17 17:24:41 +0530

    project structure

---
+++ ShareMyOffer/src/main/java/com/hackathon/ShareMyOffer/ShareMyOfferApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.ShareMyOffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMyOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMyOfferApplication.class, args);
+	}
+
+}
---
+++ ShareMyOffer/src/test/java/com/hackathon/ShareMyOffer/ShareMyOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.ShareMyOffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMyOfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
