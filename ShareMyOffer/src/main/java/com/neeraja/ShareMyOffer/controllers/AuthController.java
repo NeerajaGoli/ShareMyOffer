19b45fa58590c675503d89b306650e7fb5c558a4
Sai Srivathsa Muthyala <saisrivathsa.muthyala@zemosolabs.com>
2020-04-20 11:57:09 +0530

    Basic Login Page Added

--- ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
+++ ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
@@ -1,5 +1,21 @@
 package com.neeraja.ShareMyOffer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.neeraja.ShareMyOffer.entities.Login;
+
+@Controller
+@RequestMapping("/shareMyOffer")
 public class AuthController {
+
+	@GetMapping("/login")
+	private String loginPage(Model theModel) {
+		Login user = new Login();
+		theModel.addAttribute("user",user);
+		return "login-page";
+	}
 
 }
