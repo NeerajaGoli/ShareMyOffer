07a36dcdfffdffd6fc85f358bf956607ebf98fb2
shashankbalabhadra <shashank.balabhadra@gmail.com>
2020-04-20 07:42:25 +0530

    sign up page added

--- ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
+++ ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
@@ -1,5 +1,15 @@
 package com.neeraja.ShareMyOffer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shareMyOffer")
 public class AuthController {
 
+	@GetMapping("/page")
+	public String showSignUp() {
+		return "signup-page";
+	}
 }
