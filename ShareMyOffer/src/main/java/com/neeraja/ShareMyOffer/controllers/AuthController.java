a2283648df337145ebf9d135c8bb16a0b00a15b5
Sai Srivathsa Muthyala <saisrivathsa.muthyala@zemosolabs.com>
2020-04-20 14:51:02 +0530

    Login Page Setup

--- ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
+++ ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
@@ -3,18 +3,32 @@ package com.neeraja.ShareMyOffer.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.neeraja.ShareMyOffer.entities.Login;
+import com.neeraja.ShareMyOffer.dto.LoginDTO;
 
 @Controller
 @RequestMapping("/shareMyOffer")
 public class AuthController {
 
+	LoginDTO tempLogin = new LoginDTO();
+	
 	@GetMapping("/login")
-	private String loginPage(Model theModel) {
-		Login user = new Login();
-		theModel.addAttribute("user",user);
+	public String loginPage() {
+		return "login-page";
+	}
+	
+	@PostMapping("/validate")
+	public String validate(@ModelAttribute("login")LoginDTO login,Model model) {
+		tempLogin.setUsername("srivathsa");
+		tempLogin.setPassword("srivathsa@123");
+		if(tempLogin.getUsername().equals(login.getUsername()) && tempLogin.getPassword().equals(login.getPassword())) {
+			model.addAttribute("login",tempLogin);
+			return "home-page";
+		}
+		model.addAttribute("noMatch",true);
 		return "login-page";
 	}
 
