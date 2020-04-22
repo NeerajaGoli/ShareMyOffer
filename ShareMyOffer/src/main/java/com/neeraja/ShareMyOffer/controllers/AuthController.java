ea7630b3a416e93a7e11d50614d6bfecf20875e8
shashankbalabhadra <shashank.balabhadra@gmail.com>
2020-04-22 13:49:55 +0530

    signup and otp page

--- ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
+++ ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
@@ -1,5 +1,27 @@
 package com.neeraja.ShareMyOffer.controllers;
 
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/shareMyOffer")
 public class AuthController {
+
+	
+	
+		
+	@GetMapping("/signup")
+	public String signupPage() {
+		return "signup-page";
+	}
+	
+	@GetMapping("/otp")
+	public String verifyPage() {
+		return "otp-page";
+	}
 
 }
