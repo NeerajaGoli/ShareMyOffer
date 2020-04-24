6c78d0b71089f7a5f15162edf5354960535ee049
vangaru shanmukha <vangarushanmukha@gmail.com>
2020-04-24 17:03:59 +0530

    merges dev

--- ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
+++ ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/AuthController.java
@@ -7,43 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/shareMyOffer")
+@RequestMapping("/")
 public class AuthController {
 	
 	private static Logger logger = LoggerFactory.getLogger(AuthController.class);
 	
-	@GetMapping("/showMyLoginPage")
+	@GetMapping("")
 	public String showMyLoginPage() {
 		logger.info("In auth controller");
 		return "login-page";
 	}
 	
-	@GetMapping("/access-denied")
+	@GetMapping("access-denied/")
 	public String showAccessDenied() {
 		return "access-denied";
-	}
-	
-	@GetMapping("/login")
-	public String loginPage() {
-		return "login-page";
-	}
-	
-	
-	@GetMapping("/home")
-	public String homePage() {
-		return "home-page";
-	}
-		
-	@GetMapping("/signup")
-	public String signupPage() {
-		
-		return "signup-page";
-	}
-	
-	@GetMapping("/otp")
-	public String verifyPage() {
-		
-		return "otp-page";
 	}
 
 }
--- ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/SignupController.java
+++ ShareMyOffer/src/main/java/com/neeraja/ShareMyOffer/controllers/SignupController.java
@@ -1,20 +1,38 @@
 package com.neeraja.ShareMyOffer.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.neeraja.ShareMyOffer.dto.AddressDTO;
 import com.neeraja.ShareMyOffer.dto.UserDTO;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/signup")
+@Slf4j
 public class SignupController {
 
 	@GetMapping("/showMySignUpPage")
 	public String showMySignUpPage(Model theModel) {
 		UserDTO userDTO = new UserDTO();
+		AddressDTO addressDTO = new AddressDTO();
 		theModel.addAttribute("user", userDTO);
+		theModel.addAttribute("address", addressDTO);
+		return "signup-page";
+	}
+	
+	@GetMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("user") UserDTO user,
+									@ModelAttribute("address") AddressDTO address,
+									BindingResult theBindingResult, Model theModal) {
+		log.info(user.getFirstName());
 		return "signup-page";
 	}
 }
